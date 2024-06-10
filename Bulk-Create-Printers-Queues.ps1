56b6a42c531af5a8f0cba3ac6009239788d22e70
bitbot <48230838+ahmadaoun@users.noreply.github.com>
2024-06-10 09:30:18 +0000

    Initial commit

---
+++ Bulk-Create-Printers-Queues.ps1
@@ -0,0 +1,74 @@
+# Function to create Install.ps1 script
+function Create-InstallScript {
+    param (
+        [string]$installPath,
+        [string]$server,
+        [string]$printer
+    )
+
+    $installScript = @"
+Param(
+    [Parameter(Mandatory = `$true)]
+    [ValidateSet("Install", "Uninstall")]
+    [String[]]
+    `$Mode
+)
+`$server = "$server"
+`$PrinterName = "$printer"
+If (`$Mode -eq "Install") {
+    if ((Test-NetConnection -ComputerName `$server | Select-Object -ExpandProperty PingSucceeded)) {
+        Add-Printer -ConnectionName "\\`$server\`$PrinterName"
+    }
+}
+If (`$Mode -eq "Uninstall") {
+    Remove-Printer -Name "\\`$server\`$PrinterName"
+}
+"@
+
+    $installScript | Out-File -FilePath $installPath -Encoding utf8
+}
+
+# Function to create Detect.ps1 script
+function Create-DetectScript {
+    param (
+        [string]$detectPath,
+        [string]$server,
+        [string]$printer
+    )
+
+    $detectScript = @"
+# Detection
+`$server = "$server"
+`$PrinterName = "$printer"
+if (Test-Path "HKCU:\Printers\Connections\,,`$server,`$PrinterName") {
+    "Installed"
+} else {
+}
+"@
+
+    $detectScript | Out-File -FilePath $detectPath -Encoding utf8
+}
+
+# Get the list of printers from a CSV file
+$data = Import-Csv -Path "$PSScriptRoot\Printers.csv"
+
+# Ensure $data is treated as an array even if it has a single entry
+if ($data -isnot [System.Collections.IEnumerable]) {
+    $data = @($data)
+}
+
+foreach ($entry in $data) {
+    $printer = $entry.Printers
+    $server = $entry.Servers
+
+    $folderPath = "$PSScriptRoot\$printer"
+    $installPath = "$folderPath\Install.ps1"
+    $detectPath = "$PSScriptRoot\$printer\Detect.ps1"
+
+    # Create folder structure
+    New-Item -ItemType Directory -Path $folderPath -Force | Out-Null
+
+    # Create scripts
+    Create-InstallScript -installPath $installPath -server $server -printer $printer
+    Create-DetectScript -detectPath $detectPath -server $server -printer $printer
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# bulk-create-network-printers
-Allows the bulk creation of network printer install and detection scripts from a csv file
+# Bulk Create Network Printer Queues
+
+This script allows you to bulk create network printers on your Windows machine.
+
+## Usage
+
+1. Clone this repository to your local machine.
+2. Open a command prompt and navigate to the project directory.
+3. Run the script using the following command:
+
+.\Bulk-Create-Prints-Queues.ps1
+
+4. Sit back and relax while the script creates the network printers for you.
+5. The script will create a folder for each printer
+. The folders include an install.ps1 script and a detec.ps1 script.
+## Contributing
+
+Setup on Intune can be found here:
+
+
+Contributions are welcome! If you have any suggestions or improvements, feel free to open an issue or submit a pull request.
